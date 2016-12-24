f07e50cfdbc750bcb2b4dd751bf740b297cb9958
sofysmol <sov-95@mail.ru>
2016-12-24 03:13:08 +0300

    deleted build files

--- src/main/java/sofysmo/testcalculator/BootApplication.java
+++ src/main/java/sofysmo/testcalculator/BootApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+//
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
